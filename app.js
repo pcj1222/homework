c39e50aa6f869f8963a9ba34318117ce96992960
pcj1222 <alsgur124@gmail.com>
2020-07-20 21:36:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+//  loop 함수 구현
 function loop( clientFunc, maxNumber ){
     let count = 1;
-    while( count < maxNumber ){
+    while(count < maxNumber){
         clientFunc(count)
         count++;
     }
 }
-
-loop( function(i){
+loop(function(i){
     console.log( fizzBuzz(i) )
-}, 10 )
+}, 10)
+
+
+// 비동기 asyncLoop 함수 구현
+function asyncLoop( clientFunc, maxNumber, time ){
+    function func(){
+        let count = 1;
+        while( count < maxNumber ){
+            clientFunc(count)
+            count++;
+        }
+    }
+    
+    setTimeout(() => {
+        func()
+    }, time);
+
+    // setInterval(() => {
+    //     func()
+    // }, time);
+}
+
+asyncLoop( function(i){
+    console.log( fizzBuzz(i) )
+}, 5, 2000)
